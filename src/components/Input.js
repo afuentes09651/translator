59b4e7ae1b5c0fc1242824a608d7038fa35a3e5c
Armando <armandez09651@yahoo.com>
2021-03-23 13:49:22 -0500

    Quick Test
    Quick demonstration showing that the API is successfully called before moving on

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,12 +1,26 @@
 import React, {useState} from 'react';
 import translate from 'translate';
 
+translate.engine = 'libre'; //this allows for us to not need an API key
+
 function Input(){
-  const [input, setInput] = useState('');
+
+  //some states for our I/O
+  const [input, setInput] = useState(''); //initialized to test for testing purposes
+  const [output,setOutput] = useState('')
+
+  //This function executes the translation
+  async function execTrans(){
+    setOutput('loading translation...'); //Notifying the user that the translation is loading if it takes a while...
+    const spanish = await translate('hello world', 'es'); //test call
+    setOutput(spanish); //set the output
+  }
 
   return(
     <>
     <p>Enter Input here</p>
+    <p>{output}</p>
+    <button onClick={() => execTrans()}>Click Me</button>
     </>
   );
 }
