34918bab3a5577f4c6170564ab982347bdd84e6d
Armando <armandez09651@yahoo.com>
2021-03-23 13:35:32 -0500

    Stable Start
    Made a start that builds and displays something

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header(){
+  return(
+    <>
+      <h1>Welcome to Translator!</h1>
+    </>
+  );
+}
+
+export default Header;
---
+++ src/components/Input.js
@@ -0,0 +1,14 @@
+import React, {useState} from 'react';
+import translate from 'translate';
+
+function Input(){
+  const [input, setInput] = useState('');
+
+  return(
+    <>
+    <p>Enter Input here</p>
+    </>
+  );
+}
+
+export default Input;
